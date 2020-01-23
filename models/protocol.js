eddb1ec88dd84599bffa1626cc2b4d522a72a904
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-23 13:25:47 -0700

    changed category field to list of possible categories. changed name field to title, added fileUrl & district fields. district may not be most acurate name but works for now as I don't have a better option at the moment

--- models/protocol.js
+++ models/protocol.js
@@ -3,14 +3,24 @@ const Schema = mongoose.Schema;
 
 const protocolSchema = new Schema({
   category: {
-    type Schema.Types.ObjectId,
-    ref: "ProtocolCategory",
+    type: String,
+    enum: ["cardiac", 'general', 'medical', 'trauma']
     required: true
   },
-  name: {
+  title: {
+    type: String,
+    required: true
+  },
+  distric: {
+    type: [String],
+    required: true
+  }
+  fileUrl: {
     type: String,
     required: true
   }
+
+
 });
 
 module.exports = mongoose.model("Protocol", protocolSchema);
