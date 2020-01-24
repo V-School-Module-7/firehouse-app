c3147d839bd90513225462c98591486d6a03fc7c
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-24 12:26:59 -0700

    fixed some typos and updated category options

--- models/protocol.js
+++ models/protocol.js
@@ -4,23 +4,22 @@ const Schema = mongoose.Schema;
 const protocolSchema = new Schema({
   category: {
     type: String,
-    enum: ["cardiac", 'general', 'medical', 'trauma']
+    enum: ["cardiac", "general", "medical", "trauma", "administrative"],
     required: true
   },
   title: {
     type: String,
     required: true
   },
-  distric: {
+  district: {
     type: [String],
+    default: "lehi",
     required: true
-  }
+  },
   fileUrl: {
     type: String,
     required: true
   }
-
-
 });
 
 module.exports = mongoose.model("Protocol", protocolSchema);
