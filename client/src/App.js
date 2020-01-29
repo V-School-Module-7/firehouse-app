4da5e90260e5859d20174971deefcc11d42fd474
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-29 16:20:45 -0700

    added switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 import GlobalStyle from "./theme/GlobalStyle";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
+import LoginPage from './pages/LoginPage';
 
 const AppWrapper = styled.div``;
 
@@ -12,7 +14,10 @@ export default function App(props) {
     <AppWrapper>
       <GlobalStyle />
       <Navbar />
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/login' component={LoginPage}
+      </Switch>
     </AppWrapper>
   );
 }
