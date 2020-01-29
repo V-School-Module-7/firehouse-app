d61caee4f0f7c99ffa62cdc6695c4cd22bc4dcc4
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-29 13:02:30 -0700

    started building the login-page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginContainer = styled.div``;
+
+export default function Login(props) {
+  return (
+    <LoginContainer>
+      <h1>This is the login page!</h1>
+    </LoginContainer>
+  );
+}
