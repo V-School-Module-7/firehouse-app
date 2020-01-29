a9c9ec9d6072f739725625f9026cb2b003a6b1b4
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-29 14:05:46 -0700

    updated homepage to reflect latest design changes. had to approximate from an image until I have access to actual design files to work with

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import LehiFireLogoImg from "../assets/lehi-fire-shield-logo.png";
+import SearchBar from "../components/SearchBar";
 import Button from "../components/Button";
 
 const HomepageWrapper = styled.div`
@@ -9,17 +9,10 @@ const HomepageWrapper = styled.div`
   align-items: center;
   justify-content: center;
 `;
-const LehiFireLogo = styled.img`
-  grid-column: 1 span 2;
-  margin: 0 auto 0 auto;
-  max-width: 400px;
-  width: 95%;
-  height: auto;
-`;
 const ButtonsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-auto-rows: 60px;
+  grid-auto-rows: auto;
   grid-gap: 8px;
   width: 100vw;
   padding: 8px;
@@ -40,7 +33,7 @@ function Homepage(props) {
   const displayButtons = buttonNames.map(name => <Button text={name} />);
   return (
     <HomepageWrapper>
-      <LehiFireLogo src={LehiFireLogoImg} alt="Lehi Fire Department Logo." />
+      <SearchBar />
       <ButtonsContainer>{displayButtons}</ButtonsContainer>
     </HomepageWrapper>
   );
