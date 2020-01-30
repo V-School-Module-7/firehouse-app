69aee7db28578830720e3d58b79719500910a07c
Rob DeGeorge <rmdegeorge@gmail.com>
2020-01-29 17:14:07 -0700

    added switch and routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,21 @@ import styled from "styled-components";
 import GlobalStyle from "./theme/GlobalStyle";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
-import LoginPage from './pages/LoginPage';
+import LoginPage from "./pages/LoginPage";
 
 const AppWrapper = styled.div``;
 
-export default function App(props) {
+function App(props) {
   return (
     <AppWrapper>
       <GlobalStyle />
       <Navbar />
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/login' component={LoginPage}
+        <Route exact path="/" component={Homepage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </AppWrapper>
   );
 }
+
+export default withRouter(App);
