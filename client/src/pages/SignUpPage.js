bbcac3ced96e5650d903206b0eb93d4cf73fd6d7
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-04 15:53:29 -0700

    build layout

---
+++ client/src/pages/SignUpPage.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Input, Button } from "../components/styledComponents";
+
+const SignUpContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-auto-rows: auto;
+  grid-gap: 9px;
+  padding: 15px;
+  width: 100vw;
+`;
+
+const WillLaterBeAnImage = styled.div`
+  background-color: rgb(176, 176, 176);
+  width: 200px;
+  height: 200px;
+  grid-column: 2 / 6;
+  margin: auto;
+`;
+
+const SignUpMessage = styled.h1`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+
+const FirstNameInput = styled(Input)`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+const LastNameInput = styled(Input)`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+const EmailInput = styled(Input)`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+const PasswordInput = styled(Input)`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+const ConfirmInput = styled(Input)`
+  grid-column: 2 / 6;
+  margin: auto;
+  text-align: center;
+`;
+
+const SignUpBtn = styled(Button)`
+  grid-column: 2 / 6;
+  margin: auto;
+`;
+
+export default function SignUp(props) {
+  return (
+    <SignUpContainer>
+      <WillLaterBeAnImage />
+      <SignUpMessage>Enter your information to get started</SignUpMessage>
+      <FirstNameInput type="text" placeholder="First Name" />
+      <LastNameInput type="text" placeholder="Last Name" />
+      <EmailInput type="text" placeholder="Email" />
+      <PasswordInput type="password" placeholder="Password" />
+      <ConfirmInput type="password" placehoder="Confirm Password" />
+      <SignUpBtn>Sign Up</SignUpBtn>
+    </SignUpContainer>
+  );
+}
