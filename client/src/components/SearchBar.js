a2696139922df9afee39e2781f397873c0484c67
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-05 15:09:35 -0700

    added search icon to search bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import searchIcon from "../assets/iconfinder_icon-111-search_314478.svg";
 
 const SearchBarContainer = styled.div`
   width: 100vw;
@@ -9,6 +10,12 @@ const SearchBarContainer = styled.div`
   padding: 8px;
 `;
 const SearchInput = styled.input`
+  padding 5px;
+  padding-left: 45px;
+  background-image: url(${searchIcon});
+  background-repeat: no-repeat;
+  background-size: auto 25px;
+  background-position: 11px 11px;
   max-width: 95vw;
   width: 100%;
   margin: auto;
@@ -16,13 +23,12 @@ const SearchInput = styled.input`
   border-radius: 5px;
   border: 1px solid rbg(176, 176, 176);
   font-size: 16pt;
-  padding: 8px;
 `;
 
 export default function SearchBar(props) {
   return (
     <SearchBarContainer>
-      <SearchInput placeholder="Search" type="text" name="search" />
+      <SearchInput type="search" placeholder="Search" name="search" />
     </SearchBarContainer>
   );
 }
