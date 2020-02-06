111b6d0adcb0bef1ac23724f3b640152dcd60fbc
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-05 17:12:34 -0700

    added icons to buttons

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import apparatusIcon from "../assets/apparatus.svg";
+import drugsIcon from "../assets/drugs.svg";
+import feedbackIcon from "../assets/feedback.svg";
+import fireIcon from "../assets/fire.svg";
+import hospitalIcon from "../assets/hospital.svg";
+import hydraulicsIcon from "../assets/hydraulics.svg";
+import medicalIcon from "../assets/medical.svg";
+import protocolsIcon from "../assets/protocols.svg";
 
 const ButtonWrapper = styled.div`
   justify-self: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
   background-color: rgb(176, 176, 176);
   border-radius: 5px;
   width: 154px;
@@ -12,7 +24,11 @@ const ButtonWrapper = styled.div`
     cursor: pointer;
   }
 `;
-const ButtonIcon = styled.img``;
+const ButtonIcon = styled.img`
+  justify-self: center;
+  height: 80px
+  width: auto;
+`;
 
 const ButtonText = styled.h2`
   color: #ffffff;
@@ -20,9 +36,27 @@ const ButtonText = styled.h2`
 `;
 
 export default function Button(props) {
+  let icon;
+  if (props.text === "Protocols") {
+    icon = protocolsIcon;
+  } else if (props.text === "Fire") {
+    icon = fireIcon;
+  } else if (props.text === "Drugs") {
+    icon = drugsIcon;
+  } else if (props.text === "Medical") {
+    icon = medicalIcon;
+  } else if (props.text === "Hospital") {
+    icon = hospitalIcon;
+  } else if (props.text === "Apparatus") {
+    icon = apparatusIcon;
+  } else if (props.text === "Hydraulics") {
+    icon = hydraulicsIcon;
+  } else if (props.text === "Feedback") {
+    icon = feedbackIcon;
+  }
   return (
     <ButtonWrapper>
-      <ButtonIcon src={props.icon} />
+      <ButtonIcon src={icon} />
       <ButtonText>{props.text}</ButtonText>
     </ButtonWrapper>
   );
