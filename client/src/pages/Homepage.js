304a03a71f3fff2a63a17ce2f8f89f22faa1c659
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-06 12:10:34 -0700

    refactored to send button icons as props when creating buttons

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
 import Button from "../components/Button";
 
+import apparatusIcon from "../assets/apparatus.svg";
+import drugsIcon from "../assets/drugs.svg";
+import feedbackIcon from "../assets/feedback.svg";
+import fireIcon from "../assets/fire.svg";
+import hospitalIcon from "../assets/hospital.svg";
+import hydraulicsIcon from "../assets/hydraulics.svg";
+import medicalIcon from "../assets/medical.svg";
+import protocolsIcon from "../assets/protocols.svg";
+
 const HomepageWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -24,17 +33,21 @@ const SearchBarHomepage = styled(SearchBar)`
 
 function Homepage(props) {
   const buttonInfo = [
-    { name: "Protocols", path: "/protocols" },
-    { name: "Fire", path: "/fire" },
-    { name: "Drugs", path: "/drugs" },
-    { name: "Medical", path: "/medical" },
-    { name: "Hospital", path: "/hospital" },
-    { name: "Apparatus", path: "/apparatus" },
-    { name: "Hydraulics", path: "/hydraulics" },
-    { name: "Feedback", path: "/feedback" }
+    { name: "Protocols", path: "/protocols", icon: protocolsIcon },
+    { name: "Fire", path: "/fire", icon: fireIcon },
+    { name: "Drugs", path: "/drugs", icon: drugsIcon },
+    { name: "Medical", path: "/medical", icon: medicalIcon },
+    { name: "Hospital", path: "/hospital", icon: hospitalIcon },
+    { name: "Apparatus", path: "/apparatus", icon: apparatusIcon },
+    { name: "Hydraulics", path: "/hydraulics", icon: hydraulicsIcon },
+    { name: "Feedback", path: "/feedback", icon: feedbackIcon }
   ];
   const displayButtons = buttonInfo.map(buttonInfo => (
-    <Button text={buttonInfo.name} path={buttonInfo.path} />
+    <Button
+      text={buttonInfo.name}
+      path={buttonInfo.path}
+      icon={buttonInfo.icon}
+    />
   ));
   return (
     <HomepageWrapper>
