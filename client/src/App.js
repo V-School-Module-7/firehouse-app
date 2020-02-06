eea66a6eeb246a16d40d3218b06a635b2553406a
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-06 12:09:14 -0700

    added routes for new pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import ProtocolsPage from "./pages/Protocols";
 
 const AppWrapper = styled.div``;
 
@@ -23,6 +24,8 @@ function App(props) {
           <Route exact path="/login" component={LoginPage} />
           <Route path="/login/signin" component={SignInPage} />
           <Route path="/login/signup" component={SignUpPage} />
+          <Route exact path="/protocols" component={ProtocolsPage} />
+          {/*<Route path="/protocols/:category" component={} />*/}
         </Switch>
       </AppWrapper>
     </ThemeProvider>
