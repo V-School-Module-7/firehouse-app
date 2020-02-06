b9248d6ee7f46808759e1038b4e7bbc1cf035711
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-06 12:11:26 -0700

    built some of the page layout and basic structure. currently still a work in progress.

---
+++ client/src/pages/Protocols.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import SearchBar from "../components/SearchBar";
+import Button from "../components/Button";
+
+const ProtocolsWrapper = styled.div``;
+const SearchBarProtocols = styled(SearchBar)``;
+const ButtonsContainer = styled.div``;
+
+function Protocols(props) {
+  const buttonInfo = [
+    { name: "General", path: "/protocols/general", icon: null },
+    { name: "Cardiac", path: "/protocols/cardiac", icon: null },
+    { name: "Medical", path: "/protocols/medical", icon: null },
+    { name: "Trauma", path: "/protocols/trauma", icon: null }
+  ];
+  const displayButtons = buttonInfo.map(buttonInfo => (
+    <Button text={buttonInfo.name} path={buttonInfo.path} />
+  ));
+  return (
+    <ProtocolsWrapper>
+      <SearchBarProtocols />
+      <ButtonsContainer>{displayButtons}</ButtonsContainer>
+    </ProtocolsWrapper>
+  );
+}
+
+export default Protocols;
