e82fa2693b02a3170be356be5591bbccaa03abf7
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-13 14:18:26 -0700

    controlled inputs for form and linked signIn to the backend auth.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
-
+import { Context } from "../context/ContextProvider";
 import { Input, InputAlt, Button } from "./styledComponents";
 
 const LoginFormWrapper = styled.form`
@@ -44,20 +44,73 @@ const SignUpBtn = styled(Button)`
 `;
 
 function LoginForm(props) {
+  const { login } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleLoginSubmit(e) {
+    e.preventDefault();
+    login(email, password);
+  }
   return props.type === "SignIn" ? (
-    <LoginFormWrapper>
-      <SignInEmailInput type="email" placeholder="Email" />
-      <SignInPasswordInput type="password" placeholder="Password" />
-      <SignInBtn>Sign In</SignInBtn>
+    <LoginFormWrapper onSubmit={e => handleLoginSubmit(e)}>
+      <SignInEmailInput
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        required
+      />
+      <SignInPasswordInput
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        required
+      />
+      <SignInBtn onClick={e => handleLoginSubmit(e)}>Sign In</SignInBtn>
     </LoginFormWrapper>
   ) : (
-    <LoginFormWrapper>
-      <FirstNameInput type="text" placeholder="First Name" />
-      <LastNameInput type="text" placeholder="Last Name" />
-      <EmailInput type="email" placeholder="Email" />
-      <PasswordInput type="password" placeholder="Password" />
-      <ConfirmInput type="password" placeholder="Confirm Password" />
-      <SignUpBtn>Sign Up</SignUpBtn>
+    <LoginFormWrapper onSubmit={e => handleLoginSubmit(e)}>
+      <FirstNameInput
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+        required
+      />
+      <LastNameInput
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+        required
+      />
+      <EmailInput
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        required
+      />
+      <PasswordInput
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        required
+      />
+      <ConfirmInput
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={e => setConfirmPassword(e.target.value)}
+        required
+      />
+      <SignUpBtn onClick={e => handleLoginSubmit(e)}>Sign Up</SignUpBtn>
     </LoginFormWrapper>
   );
 }
