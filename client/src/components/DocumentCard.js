bcce1890eb34d3a9aad5caad4f774691654a0a93
Rob DeGeorge <rmdegeorge@gmail.com>
2020-02-19 15:57:50 -0700

    wrapped title in link tag that directs to proper pdf file

--- client/src/components/DocumentCard.js
+++ client/src/components/DocumentCard.js
@@ -2,12 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 const DocumentCardWrapper = styled.div``;
+const DocumentLink = styled.a``;
 
 export default function DocumentCard(props) {
   return (
     <DocumentCardWrapper>
-      <div>{props.documentInfo.link}</div>
-      <div>{props.documentInfo.title}</div>
+      <div>
+        <DocumentLink href={props.documentInfo.fileUrl}>
+          {props.documentInfo.title}
+        </DocumentLink>
+      </div>
     </DocumentCardWrapper>
   );
 }
