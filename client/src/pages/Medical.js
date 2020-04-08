6f55558f8d70c4afca90101239fdbba46200ece6
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-04-07 20:40:47 -0600

    somethings

--- client/src/pages/Medical.js
+++ client/src/pages/Medical.js
@@ -1,3 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const MedicalWrapper = styled.div``;
+const SearchBarMedical = styled(SearchBar)``;
+const ButtonsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 154px 154px;
+  grid-auto-rows: auto;
+  grid-gap: 20px;
+  width: 100vw;
+  margin: 16px auto 0px auto;
+  justify-content: center;
+`;
+
+
+function Medical(props){
+    
+}
