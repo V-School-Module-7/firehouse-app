000560d7e2bc234ae1586318b1532d98b3a76780
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-04-14 21:35:58 -0600

    finished magnesuim sulfate

--- client/src/medical-data/magnesiumSulfate.js
+++ client/src/medical-data/magnesiumSulfate.js
@@ -5,7 +5,13 @@ export const magnesiumSulfate = {
             type: "cardiac arrest",
             weights: [
                 {
-
+                    amounts: "Adult",
+                    dosage: "2gm IV over 2 min for torsades de pointes"
+                },
+                {
+                    amounts: "Pediatric",
+                    dosage: "25-50 mg/kg IV/IO for Torsades de Pointes",
+                    notes: "Max 2 grams"
                 }
             ]
         },
@@ -13,7 +19,12 @@ export const magnesiumSulfate = {
             type: "respiratory distress",
             weights: [
                 {
-
+                    amounts: "Adult",
+                    dosage: "2 grams IV over 15-30 min for sever wheezing unresponsive to Albuterol"
+                },
+                {
+                    amounts: "Pediatric",
+                    dosage: "2 grams IV over 15-30 min for sever wheezing unresponsive to Albuterol"
                 }
             ]
         },
@@ -21,7 +32,12 @@ export const magnesiumSulfate = {
             type: "seizures",
             weights: [
                 {
-                    
+                    amounts: "Adult",
+                    dosage: "For Females with advanced pregnancy seizures - 4 grams IM or 4 grams over 15-30 min IV/IO"
+                },
+                {
+                    amounts: "Pediatric",
+                    dosage: "Contact OLMC!"
                 }
             ]
         }
