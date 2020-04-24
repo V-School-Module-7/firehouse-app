5568908a9140821f1ba44ca1ab345bb37ad5834f
Marcielo Bell <marcielobell@gmail.com>
2020-04-23 23:54:01 -0700

    finished medical data from pages 12-23 with notes

--- client/src/medical-data/phenergan.js
+++ client/src/medical-data/phenergan.js
@@ -7,11 +7,17 @@ export const phenergan = {
                 {
                     amount: "adult",
                     dosage: "IV/IO: 12.5-25 mg if SBP>100",
-                    notes: "Dilute with 10 mL of NS and push slowly over 60 sec with a wide open IV running to dilute it as it is administered | IM: 25 mg if no vascular access"
+                    notes: [
+                        "Dilute with 10mL of NS and push slowly over 60 sec with a wide open IV running to dilute it as it is administered",
+                        "IM: 25mg if no vascular access"
+                    ]
                 },
                 {
                     amount:"pediatric",
-                    dosage: "Not recommended."
+                    dosage: "Not recommended",
+                    notes: [
+                        ""
+                    ]
                 }
             ]
         }
