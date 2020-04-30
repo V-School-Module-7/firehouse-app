9d9dcb8bc6392bda5f9782b50f69a82f8a4de5a9
Marcielo Bell <marcielobell@gmail.com>
2020-04-30 11:16:31 -0700

    Medical Router included

--- routes/medDataRouter.js
+++ routes/medDataRouter.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const medDataRouter = express.Router();
-const MedicalData = require("../models/medicalData");
+const Medical = require("../models/medical");
 
-medDataRouter.route("/").get((req, res, next) => {
-  MedicalData.find((err, medicalData) => {
-    if (err) {
-      res.status(500);
-      return next(err);
-    }
-    return res.status(200).send(medicalData);
+medDataRouter
+  .route("/") //get all medical data
+  .get((req, res, next) => {
+    Medical.find((err, medicalData) => {
+      if (err) {
+        res.status(500);
+        return next(err);
+      }
+      return res.status(200).send(medicalData);
+    });
+  })
+  //post new medical data to DB
+  .post((req, res, next) => {
+    newMedical = new Medical(req.body);
+
+    newMedical.save((err, medData) => {
+      if (err) {
+        res.status(500);
+        return next(err);
+      }
+      return res.status(201).send(medData);
+    });
   });
-});
-//get all medData by condition
-medDataRouter.route("/condition/:condition").get((req, res, next) => {
-  MedicalData.find({ category: req.params.condition }, (err, medicalData) => {
+//get all medData by medication
+medDataRouter.route("/medications/:medications").get((req, res, next) => {
+  Medical.find({ medications: req.params.medications }, (err, medicalData) => {
     console.log(medicalData);
     if (err) {
       res.status(500);
       return next(err);
     }
-    return res.status(500).send(medicalData);
+    return res.status(200).send(medicalData);
   });
 });
 
