4b784f7df27d234336e17b9bdd1fdd6e2f27dc30
Marcielo Bell <marcielobell@gmail.com>
2020-05-07 11:39:51 -0700

    Completed migration of medical data to MongoDB

--- processMedData.js
+++ processMedData.js
@@ -6,5 +6,9 @@ fs.readdir("./client/src/medical-data", (err, medications)=> {
     const medicalDataCollection = medications
     .filter( file => file != 'index.js')
     .map(file => require(`./client/src/medical-data/${file}`))
-    console.log(medicalDataCollection)
+    fs.mkdir("./medical-output", () => console.log("Created new folder 'medical-output'"))
+    fs.writeFile("./medical-output/data.json", JSON.stringify(medicalDataCollection), () => {
+        console.log("Written to medical-output.json file")
+    })
+
 })
