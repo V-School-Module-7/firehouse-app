0f5dcc5730512a1d8b19e03a3b61e4308e23c1ba
zmcentire <mcentirezach@gmail.com>
2020-08-03 18:56:41 -0600

    Drugs button info

--- client/src/pages/Drugs.js
+++ client/src/pages/Drugs.js
@@ -27,3 +27,63 @@ const DrugButtonsContainer = styled.div`
     background-color:  #D8D8D8;
 `;
 
+
+function Drugs(props) {
+    const amiodaroneInfo = [
+        { name: "Cardiac Arrest", path: "/", icon: null},
+        { name: "Post Cardiac Arrest/Return of Spontaneous Circulation ROSC", path: "/", icon: null},
+        { name: "Tachycardia", path: "/", icon: null}
+    ];
+    const amiodaroneButtons = amiodaroneInfo.map((amiodaroneInfo, index) => (
+        <Button 
+            key={amiodaroneInfo + index}
+            text={amiodaronInfo.name}
+            path={amiodaroneInfo.path}
+        />
+    ));
+    const atropineInfo = [
+        { name: "Beta Blocker Toxicity", path: "/", icon: null},
+        { name: "Bradycardia", path: "/", icon: null}
+    ];
+    const atropineButtons = atropineInfo.map((atropineInfo, index) => (
+        <Button 
+            key={atropineInfo + index}
+            text={atropineInfo.name}
+            path={atropineInfo.path}
+        />
+    ));
+    const bipapInfo = [
+        { name: "Congestive Heart Failure/Pulmonary Edema", path: "/", icon: null},
+        { name: "Respiratory Distress/Airway & Tracheostomy Management", path: "/", icon: null}
+    ];
+    const bipapButtons = bipapInfo.map((bipapInfo, index) => (
+        <Button 
+            key={bipapInfo + index}
+            text={bipapInfo.name}
+            path={bipapInfo.path}
+        />
+    ));
+    const dextroseInfo = [
+        { name: "Hypoglycemia/Altered Mental Status/Violent Patient", path: "/", icon: null},
+        { name: "Newborn Resuscitation", path: "/", icon: null}
+    ];
+    const dextroseButtons = dextroseInfo.map((dextroseInfo, index) => (
+        <Button 
+            key={dextrose + index}
+            text={dextroseInfo.name}
+            path={dextrose.path}
+        />
+    ));
+    
+
+    return(
+        <DrugsWapper>
+            <SearchBarDrugs />
+            <ButtonsContainer>
+
+            </ButtonsContainer>
+        </DrugsWapper>
+    )
+}
+
+export default Drugs
