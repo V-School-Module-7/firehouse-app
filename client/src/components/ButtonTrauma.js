b8a7aad97544db22e62ac2208b3a857283ce0671
JUAN ARELLANO MARTINEZ <jmanuelarellano@JUAN-ARELLANO-MARTINEZs-iMac.local>
2020-10-02 14:51:54 -0400

    update

---
+++ client/src/components/ButtonTrauma.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const ButtonLink = styled(Link)`
+  text-decoration: none;
+`;
+const ButtonWrapper = styled.div`
+  justify-self: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  background-color: blue;
+  border-radius: 5px;
+  width: 240px;
+  height: 125px;
+  position: relative;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const ButtonText = styled.div`
+  height: 32px;
+  width: 120px;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 15px;
+  text-align: center;
+  margin: auto;
+  color: #EFFF19;
+`;
+
+export default function ButtonTrauma(props) {
+  return (
+    <ButtonLink to={props.path}>
+      <ButtonWrapper>
+        <ButtonText>{props.text}</ButtonText>
+      </ButtonWrapper>
+    </ButtonLink>
+  );
+}
--- client/src/pages/Trauma.js
+++ client/src/pages/Trauma.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import SearchBar from '../components/SearchBar';
 import Button from '../components/Button';
+import ButtonTrauma from '../components/ButtonTrauma';
 
 const TraumaWrapper = styled.div ``;
 const SearchBarTrauma = styled(SearchBar)``;
 const ButtonsContainer = styled.div`
 display: grid;
-  grid-template-columns: 154px 154px;
+  grid-template-columns: 135px 135px;
   grid-auto-rows: auto;
-  grid-gap: 20px;
+  grid-gap: 130px;
   width: 100vw;
   margin: 16px auto 0px auto;
   justify-content: center;
@@ -17,24 +18,30 @@ display: grid;
 
   function Trauma(props) {
       const ButtonInfo = [
-          { name: '1. Trauma Patient Care Guidelines', path: '/protocols/trauma', icon: null },
-          { name: '2. Utah Trauma Field Triage Guidelines', path: '/protocols/trauma', icon: null },
-          { name: '3. Amputations', path: '/protocols/trauma', icon: null },
-          { name: '4. Burns - Electrical/Thermal', path: '/protocols/trauma', icon: null },
-          { name: '5. Chest Trauma', path: '/protocols/trauma', icon: null },
-          { name: '6. Crush Injuries', path: 'protocols/trauma', icon: null },
-          { name: '7. Head Injury(Traumatic Brain Injury)', path: '/protocols/trauma', icon: null },
-          { name: '8. Hemorrhage Control and Soft Tissue Injuries', path: '/protocols/trauma', icon: null },
-          { name: '9. Non-Accidental Trauma/Abuse', path: '/protocols/trauma', icon: null },
-          { name: '10. Skeletal Injuries', path: '/protocols/trauma', icon: null },
-          { name: '11. Snake Bites', path: '/protocols/trauma', icon: null}
+          { name: 'Trauma Patient Care Guidelines', path: '/protocols/trauma', icon: null },
+          { name: 'Utah Trauma Field Triage Guidelines', path: '/protocols/trauma', icon: null },
+          { name: 'Amputations', path: '/protocols/trauma', icon: null },
+          { name: 'Burns - Electrical/Thermal', path: '/protocols/trauma', icon: null },
+          { name: 'Chest Trauma', path: '/protocols/trauma', icon: null },
+          { name: 'Crush Injuries', path: 'protocols/trauma', icon: null },
+          { name: 'Head Injury(Traumatic Brain Injury)', path: '/protocols/trauma', icon: null },
+          { name: 'Hemorrhage Control and Soft Tissue Injuries', path: '/protocols/trauma', icon: null },
+          { name: 'Non-Accidental Trauma/Abuse', path: '/protocols/trauma', icon: null },
+          { name: 'Skeletal Injuries', path: '/protocols/trauma', icon: null },
+          { name: 'Snake Bites', path: '/protocols/trauma', icon: null}
       ];
 
       const displayButtons = ButtonInfo.map((buttonInfo, index) => {
          return <Button
           key={buttonInfo + index}
           text={buttonInfo.name}
+          path={buttonInfo.path} />,
+
+          <ButtonTrauma
+          key={buttonInfo + index}
+          text={buttonInfo.name}
           path={buttonInfo.path} />
+          
       });
 
       return (
